8e79520ab74b19256aca1a5345e46da331a7ffea
Mateus Gustavo <githubmateus@gmail.com>
2023-07-26 20:31:59 -0300

    Melhorias no Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import { BsLinkedin, BsGithub, BsBoxArrowInUpRight } from 'react-icons/bs';
 
-interface Props {
+interface buttonProps {
   text: string;
   type: string;
   github?: string;
   link?: string;
 }
 
-export default function Button({ text, type, link }: Props) {
+export default function Button({ text, type, link }: buttonProps) {
   const renderIcon = () => {
     if (type === 'linkedin') {
       return <BsLinkedin size={20} />;
@@ -27,12 +27,12 @@ export default function Button({ text, type, link }: Props) {
   return (
     <button
       onClick={handleClick}
-      className="text-md group  relative w-full rounded-md bg-color1 pb-4 pt-4 font-sans font-bold text-white"
+      className="text-md max-w-[380px] group relative w-full rounded-md bg-color1 pb-4 pt-4 font-sans font-bold text-white"
     >
-      <div className="absolute top-0 h-full w-0 rounded-md bg-color2 transition-all duration-300 group-hover:w-full"></div>
-      <div className="relative z-20 m-auto flex h-max w-max items-center">
+      <div className="absolute top-0 h-full w-0 rounded-md bg-color2 transition-all duration-200 group-hover:w-full"></div>
+      <div className="relative z-20 m-auto flex h-max w-max items-center gap-2">
         {renderIcon()}
-        <span className="ml-2 mr-2 font-sans text-sm text-white">{text}</span>
+        <span className=" font-sans text-sm text-white">{text}</span>
       </div>
     </button>
   );
