36c56402f8a227ab5b9b03af68a921822f62c320
Mateus Gustavo <githubmateus@gmail.com>
2023-08-04 01:47:26 -0300

    Melhorias no conteudo

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,15 +6,19 @@ export default function AboutSection() {
       <div className="m-auto flex  h-max w-full max-w-[1400px] justify-between pl-8 pr-8 display1:pl-6 display1:pr-6 display2:flex-col-reverse display2:items-center">
         <div className="h-max w-full max-w-[700px] display2:max-w-[800px]">
           <div className="flex h-max w-full flex-col gap-8">
-            <span className="font-sans text-4xl font-semibold text-white">Um pouco sobre mim 👋</span>
+            <span className="font-sans text-4xl font-semibold text-white">Sobre mim 👋</span>
             <p className="font-sans text-lg leading-8 text-[#D7D1D1]">
               Olá! Sou o Mateus Gustavo, nascido em 2004, e minha curiosidade pelo desenvolvimento de softwares me levou
               ao vasto mundo da programação. Escolhi me especializar no desenvolvimento de interfaces de usuário, também
               conhecido como front-end. Investi meu tempo em aprender tecnologias como JavaScript, CSS, ReactJS, entre
               outras. Com disciplina diária, estudei e apliquei meus conhecimentos em projetos pessoais.
             </p>
+            <span className="font-sans text-4xl font-semibold text-white">Meu objetivo</span>
+            <p className="font-sans text-lg leading-8 text-[#D7D1D1]" >
+              Atualmente, estou em busca de uma oportunidade como desenvolvedor front-end para aplicar meus conhecimentos, ajudar a empresa/projeto a seguir evoluindo e trabalhar em equipe com outros desenvolvedores, designers, entre outros. Desejo crescer profissionalmente nessa área e contribuir para o sucesso do projeto.
+            </p>
             <span className="font-sans text-4xl font-semibold text-white">Minhas habilidades</span>
-            <div className="flex w-full flex-wrap gap-4">
+            <div className="flex w-full flex-wrap gap-4 display1:justify-center">
               <TecnologyCard icon="/skills-icons/html.svg" name="Html" />
               <TecnologyCard icon="/skills-icons/css.svg" name="Css" />
               <TecnologyCard icon="/skills-icons/javascript.svg" name="Javascript" />
@@ -24,32 +28,11 @@ export default function AboutSection() {
               <TecnologyCard icon="/skills-icons/figma.svg" name="Figma" />
               <TecnologyCard icon="/skills-icons/tailwind.svg" name="Tailwind" />
               <TecnologyCard icon="/skills-icons/git.svg" name="Git" />
+              <TecnologyCard icon="/skills-icons/github.svg" name="GitHub" />
+              <TecnologyCard icon="/skills-icons/graphql.svg" name="GraphQL" />
               <TecnologyCard icon="/skills-icons/linux.svg" name="Linux" />
             </div>
-            <span className="font-sans text-4xl font-semibold text-white">Meus diferenciais</span>
-            <ul className="flex h-max w-full list-disc flex-col space-y-4 pl-6">
-              <li>
-                Tenho uma forte dedicação e foco ao realizar minhas tarefas, sempre buscando executá-las com qualidade e
-                eficiência.
-              </li>
-              <li>
-                Sou uma pessoa comunicativa e valorizo a oportunidade de ouvir e aprender com pessoas mais experientes,
-                sempre aberto a receber dicas e ensinamentos.
-              </li>
-              <li>
-                Tenho um alto nível de organização, o que contribui para a minha produtividade e efetividade no
-                trabalho.
-              </li>
-              <li>
-                Possuo habilidade em aprender rapidamente novas tecnologias, como frameworks e bibliotecas, de forma
-                autodidata.
-              </li>
-              <li>Em relação a código, busco sempre escrever um código limpo e de fácil compreensão.</li>
-              <li>
-                Busco sempre compreender claramente cada linha de código que escrevo, em vez de apenas fazê-lo
-                funcionar.
-              </li>
-            </ul>
+
           </div>
         </div>
         <div className="h-[400px] w-full max-w-[300px] rounded-xl bg-[url('/assets/foto.png')] bg-cover bg-center display2:mb-10 "></div>
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,10 +6,10 @@ export default function ProjectSections() {
       id="projects"
       className="flex h-max w-full flex-col place-content-center gap-10 bg-color3 pb-16 pl-4 pr-4 pt-16 display1:gap-6 display1:pb-12 display1:pt-12"
     >
-      <span className="text-center font-sans text-4xl font-bold text-white display1:text-3xl">Meus projetos</span>
+      <span className="text-center font-sans text-4xl font-bold text-white display1:text-3xl">Meus projetos 🔥</span>
       <div className="m-auto flex h-max max-w-[1400px] flex-wrap justify-center">
         <ProjectCard
-          name="Capputeeno 🛒"
+          name="Capputeeno"
           description="
           O projeto é um desafio para uma vaga de front-end na Rocketseat, que tem como objetivo a criação de uma mini loja virtual com vários recursos legais."
           image="/projects/image05.png"
@@ -17,7 +17,7 @@ export default function ProjectSections() {
           github="https://github.com/MateusGustavo22/frontend-challeng"
         />
         <ProjectCard
-          name="Xmoedas 💸"
+          name="Xmoedas"
           link="https://xmoedas.com.br/"
           description="O xmoedas.com.br permiti que você fique atualizado da cotação atual do dólar, euro, libra e ontre outras moedas. Também é possível realizar a converção para o real brasileiro."
           image="/projects/image03.png"
@@ -25,14 +25,14 @@ export default function ProjectSections() {
           github="https://github.com/MateusGustavo22/conversor-moedas"
         />
         <ProjectCard
-          name="Megaflix  🎥"
+          name="Megaflix"
           description="O Megaflix é um projeto de front-end para um serviço de streaming de filmes e séries, com categorias, scroll horizontal e busca utilizando a API do TMDB."
           image="/projects/image01.png"
           tags={['React', 'Typescript', 'Next', 'Tailwind']}
           github="https://github.com/MateusGustavo22/megaflix"
         />
         <ProjectCard
-          name="Weather App 🌦"
+          name="Weather App"
           link="https://previsao-do-tempo-flame.vercel.app/"
           description="Este projeto front-end consiste em um website de previsão do tempo, onde você pode visualizar informações climáticas atuais e dos próximos 5 dias de qualquer cidade no mundo."
           image="/projects/image02.png"
@@ -40,7 +40,7 @@ export default function ProjectSections() {
           github="https://github.com/MateusGustavo22/previsao-do-tempo"
         />
         <ProjectCard
-          name="Starlink clone  🛰"
+          name="Starlink clone"
           link="https://starlink-clone.vercel.app/"
           description="Clone da página do starlink 100% responsiva com menu lateral utiliziando React e Tailwind. "
           image="/projects/image04.png"
